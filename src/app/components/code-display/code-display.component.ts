b962fa348dc960a45773e61d2316925b0a263251
CENTURION\a.harney <a.harney@centurion.net.au>
2024-07-15 13:47:31 +0800

    first list of stratagem codes

--- src/app/components/code-display/code-display.component.ts
+++ src/app/components/code-display/code-display.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { stratagemCode } from '../../models/stratagem-codes';
+import { CommonModule } from '@angular/common';
+import { inputDirection } from '../../models/stratagem-inputs';
 
 @Component({
   selector: 'app-code-display',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './code-display.component.html',
   styleUrl: './code-display.component.scss'
 })
 export class CodeDisplayComponent {
+
+  public inputDirection = inputDirection;
+
+  @Input() code!: stratagemCode;
+
+  ngOnInit(): void {
+    console.log(this.code);
+  }
 
 }
